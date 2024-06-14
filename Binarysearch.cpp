396dedd356ff4932ff40625f4ae9c2d4ab94be19
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-06-14 10:18:28 +0700

    Memasukan void preorder

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -77,4 +77,16 @@ public:
 		}
 	}
 
+    void preorder(Node* ptr) // function to perform inorder traversal
+	{
+		if (ROOT == NULL) {
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			cout << ptr->info << " "; // root
+			preorder(ptr->leftchild); // left
+			preorder(ptr->rightchild); // right
+		}
+	}
 	};
